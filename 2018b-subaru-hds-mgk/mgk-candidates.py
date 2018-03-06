07d948e43cb79afa8e7b212adc501a6ffa27b6b9
Andy Casey <andycasey@gmail.com>
2018-03-07 10:08:51 +1100

    scripts  etc

---
+++ 2018b-subaru-hds-mgk/mgk-candidates.py
@@ -0,0 +1,147 @@
+import requests
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from collections import OrderedDict
+
+candidates = Table.read("mgk-candidates.csv")
+
+# Only propose to observe V < 14
+keep = (~candidates["Vmag"].mask) \
+     * (candidates["Vmag"] < 14)
+
+candidates = candidates[keep]
+candidates.sort("ra")
+
+def exposure_time(vmag):
+
+    exps = dict([
+        (9, 1200),
+        (10, 1300),
+        (11, 1400),
+        (12, 1500),
+        (13, 1600)
+    ])
+
+    for mag, exp in exps.items():
+        if mag > vmag:
+            print(vmag, mag, exp)
+            return exp
+
+    return exp
+
+#\target{}{}{}{}
+remove_sep = lambda s: s.replace(":", "").replace(".", "")
+
+output = []
+total_exp = 0
+
+for candidate in candidates:
+    
+    position = SkyCoord(candidate["ra"], candidate["dec"], unit=(u.deg, u.deg))
+
+    ra, dec = position.to_string("hmsdms", sep=":").split()
+    identifier = "J{}{}".format(remove_sep(ra[:11]), remove_sep(dec[:11]))
+
+    vmag = candidate["Vmag"]
+    t_exp = exposure_time(vmag)
+
+    try:
+        comment = "V={vmag:.1f} $t_{{\\rm exp}} = {exp:.0f}$ s".format(
+            vmag=vmag, exp=t_exp)
+
+    except TypeError:
+        comment = "V=\\todo{{XX.X}} $t_{{\\rm exp}} = XX$ s"
+
+    line = "\\target{{{identifier}}}{{{ra}}}{{{dec}}}{{{comment}}}".format(
+        identifier=identifier, ra=ra[:11], dec=dec[:11], comment=comment)
+
+    output.append(line)
+
+    total_exp += t_exp
+
+with open("candidates.tex", "w") as fp:
+    fp.write("\n".join(output))
+
+
+# Check SMOKA for each candidate.
+smoka_url = "http://smoka.nao.ac.jp/fssearch"
+
+N = len(candidates)
+
+for i, candidate in enumerate(candidates):
+
+    print(i, N)
+
+    #ra = "21h03m52.11s"
+    #dec = "-29d42m50.2s"
+    ra = "{:.5f}".format(candidate["ra"])
+    dec = "{:.5f}".format(candidate["dec"])
+
+    data  = [
+        ("object", ""),
+        ("resolver", "SIMBAD"),
+        ("coordsys", "Equatorial"),
+        ("equinox", "J2000"),
+        ("fieldofview", "auto"),
+        ("RadOrRec", "radius"),
+        ("longitudeC", ra),
+        ("latitudeC", dec),
+        ("radius", "0.08"),
+        ("longitudeF", ""),
+        ("latitudeF", ""),
+        ("longitudeT", ""),
+        ("latitudeT", ""),
+        ("date_obs", ""),
+        ("exptime", ""),
+        ("observer", ""),
+        ("prop_id", ""),
+        ("frameid", ""),
+        ("exp_id", ""),
+        ("dataset", ""),
+        ("asciitable", "Ascii"),
+        ("frameorshot", "Frame"),
+        ("action", "Search"),
+        ("instruments", "HDS"),
+        ("multiselect_0", "HDS"),
+        ("obs_mod", "IMAG"),
+        ("obs_mod", "SPEC"),
+        ("obs_mod", "IPOL"),
+        ("multiselect_1", "IMAG"),
+        ("multiselect_1", "SPEC"),
+        ("multiselect_1", "IPOL"),
+        ("data_typ", "OBJECT"),
+        ("multiselect_2", "OBJECT"),
+        ("obs_cat", "Science+Observation"),
+        ("multiselect_3", "Science+Observation"),
+        ("bandwidth_type", "FILTER"),
+        ("band", ""),
+        ("dispcol", "FRAMEID"),
+        ("dispcol", "DATE_OBS"),
+        ("dispcol", "FITS_SIZE"),
+        ("dispcol", "OBS_MODE"),
+        ("dispcol", "DATA_TYPE"),
+        ("dispcol", "OBJECT"),
+        ("dispcol", "WVLEN"),
+        ("dispcol", "DISPERSER"),
+        ("dispcol", "ECLLONG"),
+        ("dispcol", "ECLLAT"),
+        ("dispcol", "UT_START"),
+        ("dispcol", "EXPTIME"),
+        ("orderby", "FRAMEID"),
+        ("diff", "100"),
+        ("output_equinox", "J2000"),
+        ("from", "0"),
+    ]
+
+    check = requests.post(smoka_url, data=data)
+
+    assert check.ok
+
+    if "No matching frame were found" in check.text:
+        print("No data")
+
+    else:
+        # You gotta justify this in your proposal. How much data exists?
+        raise a
+
